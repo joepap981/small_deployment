6aa943794c6d48ce4d7495451a4dd15301cbc23d
root <joepap981@gmail.com>
2017-10-28 20:11:45 +0000

    "Enabled Mongo Replication cluster"

--- install_small_deployment.sh
+++ install_small_deployment.sh
@@ -6,8 +6,8 @@ export ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do
 
 #install datastores server
 echo "Now installing datastores server..."
-sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-datastores.yml
+sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-datastores.yml -vvvv
 
 #install stateless server
 echo "Now installing Stateless server..."
-sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-stateless.yml
+sudo ANSIBLE_ROLES_PATH=/var/tmp/configuration/playbooks/roles ANSIBLE_LIBRARY=/var/tmp/configuration/playbooks/library ANSIBLE_JINJA2_EXTENSIONS=jinja2.ext.do  ansible-playbook -i inventory.ini -e server_var.yml playbooks/edx-stateless.yml -vvvv
